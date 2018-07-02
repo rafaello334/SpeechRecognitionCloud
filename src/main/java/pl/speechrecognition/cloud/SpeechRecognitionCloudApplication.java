c675095f571e13e0a5a80b7379786144deac3644
Rafał Sokalski <rafaello334@gmail.com>
2018-07-02 19:22:12 +0200

    Added recognizing wav file

--- src/main/java/pl/speechrecognition/cloud/SpeechRecognitionCloudApplication.java
+++ src/main/java/pl/speechrecognition/cloud/SpeechRecognitionCloudApplication.java
@@ -1,10 +1,19 @@
 package pl.speechrecognition.cloud;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import pl.speechrecognition.cloud.config.SpeechRecognizer;
+
 @SpringBootApplication
 public class SpeechRecognitionCloudApplication {
+	public static Log logger = LogFactory.getLog(SpeechRecognitionCloudApplication.class);
+
+	@Autowired
+	SpeechRecognizer speechRecognizer;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpeechRecognitionCloudApplication.class, args);
